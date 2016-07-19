07fe51ddd9af27d44ad633401d1acc3ee262353a
Pedro de Melo Strabeli <pedrostrabeli@gmail.com>
2016-07-19 09:07:50 -0300

    bugfix

--- public/js/controllers/cart_ctrl.js
+++ public/js/controllers/cart_ctrl.js
@@ -5,28 +5,31 @@ angular.module('systennis')
 		
 		var user={id_user:1};
 		if(user){
+			console.log('Usuario logado')
 			//checa se existe algo no carrinho local.
-			if (cartService.getProducts()!=[]){
+			if (cartService.getProducts()[0]!= null){
+				//console.log(cartService.getProducts()!==[])
 				Checker();
+				console.log('checker')
 			}
-			else if(cartService.getProducts()==[])
-			$http.get('/cart/1').then(function(response){
-				
-				$scope.cart = response.data;
-				//console.log(response.data);
-			});//.then(function(){
+			else {
+				$http.get('/cart/1').then(function(response){
+					
+					$scope.cart = response.data;
+					console.log(response.data);
+				});//.then(function(){
+			}
 		}
 		else{
 			$scope.cart = cartService.getProducts();
 		}
 		
 		//FAZER IR PARA O COOKIE
-		//FAZER A COMPARAÇÂO DO CARRINHO NO BANCO COM O CARRINHO LOCAL E FAZER O INSERT
 
 		function Checker(){
 			var lista=cartService.getProducts();
-			lista.forEach(function(item, index){
-
+			while(lista[0]!=null){
+				item=lista.pop();
 				itemCart={
 					id_prod: item.id_prod,
 					id_cliente: user.id_user,
@@ -50,6 +53,6 @@ angular.module('systennis')
 						}
 						
 					});	
-			})
+			}
 		};
 	});
