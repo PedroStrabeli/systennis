1106adae08dedacbce85e231167597c9be966496
Pedro de Melo Strabeli <pedrostrabeli@gmail.com>
2016-07-22 01:28:39 -0300

    novo template implementado. acredito que todas as paginas.

--- public/js/config.js
+++ public/js/config.js
@@ -8,19 +8,43 @@ var config = ['$urlRouterProvider', '$stateProvider', '$locationProvider', funct
     $stateProvider
         .state('catalog', {
               url: "/",
-              templateUrl: "pages/catalog.html",
+              templateUrl: "pages/template/shop-category.html",
               controller: 'catalog_ctrl',
               data: { pageTitle: 'Pagina Principal' }
           })
         .state('cart', {
               url: "/carrinho",
-              templateUrl: "pages/cart.html",
+              templateUrl: "pages/template/shop-basket.html",
               controller: 'cart_ctrl',
               data: { pageTitle: 'Carrinho' }
           })
+        .state('checkout1', {
+              url: "/endereco_entrega",
+              templateUrl: "pages/template/shop-checkout1.html",
+              controller: '',
+              data: { pageTitle: 'Endereço de Entrega' }
+          })
+        .state('checkout2', {
+              url: "/modo_entrega",
+              templateUrl: "pages/template/shop-checkout2.html",
+              controller: '',
+              data: { pageTitle: 'Modo da Entrega' }
+          })
+        .state('checkout3', {
+              url: "/pagamento",
+              templateUrl: "pages/template/shop-checkout3.html",
+              controller: '',
+              data: { pageTitle: 'Pagamento' }
+          })
+        .state('checkout4', {
+              url: "/review_order",
+              templateUrl: "pages/template/shop-checkout4.html",
+              controller: '',
+              data: { pageTitle: 'Reveja seu Pedido' }
+          })
         .state('prod_detail', {
               url: "/detalheProduto",
-              templateUrl: "pages/prod_detail.html",
+              templateUrl: "pages/template/shop-detail.html",
               controller: 'prod_detail_ctrl',
               data: { pageTitle: 'Detalhes' },
               param:{ detail: null }
@@ -31,30 +55,37 @@ var config = ['$urlRouterProvider', '$stateProvider', '$locationProvider', funct
               controller: 'cadastro_prod_ctrl',
               data: { title: 'Cadastro de Produtos' },
           })
-        .state('new_index', {
-              url: "/new_index",
-              templateUrl: "pages/template/shop-category.html",
-              controller: 'catalog_ctrl',
-              data: { title: 'Systennis' },
+        .state('cadastro_cliente', {
+              url: "/cadastro",
+              templateUrl: "pages/template/customer-register.html",
+              controller: '',
+              data: { title: 'Cadastro de Cliente' },
           })
-        .state('new_prod_detail', {
-              url: "/new_detalheProduto",
-              templateUrl: "pages/template/shop-detail.html",
-              controller: 'prod_detail_ctrl',
-              data: { pageTitle: 'Detalhes' },
-              param:{ detail: null }
+        .state('perfil', {
+              url: "/perfil",
+              templateUrl: "pages/template/customer-account.html",
+              controller: '',
+              data: { title: 'Perfil' },
           })
-        .state('new_cart', {
-              url: "/carrinho",
-              templateUrl: "pages/template/shop-basket.html",
-              controller: 'cart_ctrl',
-              data: { pageTitle: 'Carrinho' }
+        .state('order_detail', {
+              url: "/detalhes_pedido",
+              templateUrl: "pages/template/customer-order.html",
+              controller: '',
+              data: { title: 'Detalhes de Pedido' },
           })
-        // .state('outlook', {
-        //     url: "/outlook",
-        //     templateUrl: "page/outlook/outlook.html",
-        //     data: { pageTitle: 'Outlook view', specialClass: 'fixed-sidebar' }
-        // })
+        .state('orders', {
+              url: "/pedidos",
+              templateUrl: "pages/template/customer-orders.html",
+              controller: '',
+              data: { title: 'Pedidos' },
+          })
+        .state('wishlist', {
+              url: "/wishlist",
+              templateUrl: "pages/template/customer-wishlist.html",
+              controller: '',
+              data: { title: 'Lista de Desejos' },
+          })
+
 }];
     
 
--- public/js/controllers/catalog_ctrl.js
+++ public/js/controllers/catalog_ctrl.js
@@ -1,11 +1,24 @@
 angular.module('systennis')
 
+	.filter('offset', function() {
+	  return function(input, start) {
+	    start = parseInt(start, 10);
+	    return input.slice(start);
+	  };
+	})
 	.controller('catalog_ctrl',function($scope, $http, productService, cartService){
 		$scope.title = 'Systennis'
 
 		$scope.pagename='Catálogo';
 		$http.get('/catalog').success(function(response){			
 			$scope.result = response;
+			$scope.currentPage = 0;
+		    $scope.pageSize = 10;
+		    //$scope.data = [];
+		    $scope.numberOfPages=function(){
+	        return Math.ceil($scope.result.length/$scope.pageSize);                
+	    }
+	    
 		});
 
 		$scope.sendDetail = function(currObj){
@@ -15,6 +28,7 @@ angular.module('systennis')
     	$scope.addProduct = function(currObj){
         	cartService.addProduct(currObj);
     	};
+
 
 		$scope.searchProduct = function(){
 			console.log($scope.search.opt);
